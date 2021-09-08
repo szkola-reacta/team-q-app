7fdd0fe3c352b9230730713a2e00150098728309
KrzGas <kgasik@gmail.com>
2021-09-08 19:55:04 +0200

    promo window added to view

---
+++ src/components/PromoWindow/PromoWindow.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import img from "../../assets/mrrobot.jpg"
+
+export default function PromoWindow() {
+  return (
+    <div className="promo-window">
+      <img
+        src={img}
+        alt="Mr.robot"
+        className="promo-window__image"
+      />
+      <div className="promo-window__description">
+          <p className="promo-window__description-title">Mr.Robot</p>
+          <p className="promo-window__description-time">20:00</p>
+      </div>
+    </div>
+  );
+}
