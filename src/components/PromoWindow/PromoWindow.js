ae44cf692152217b24d2e99bddda022b9046c239
KrzGas <kgasik@gmail.com>
2021-09-10 12:02:15 +0200

    adjust title and add description from promo to tiles

--- src/components/PromoWindow/PromoWindow.js
+++ src/components/PromoWindow/PromoWindow.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-import img from "../../assets/mrrobot.jpg"
+import img from "../../assets/mrrobot.jpg";
 
 export default function PromoWindow() {
   return (
     <div className="promo-window">
       <p className="promo-window__title">Recommended title for You</p>
-      <img
-        src={img}
-        alt="Mr.robot"
-        className="promo-window__image"
-      />
+      <img src={img} alt="Mr.robot" className="promo-window__image" />
       <div className="promo-window__description">
-          <p className="promo-window__description-title">Mr.Robot</p>
-          <p className="promo-window__description-time">20:00</p>
+        <p className="promo-window__description-title">Mr.Robot</p>
+        <p className="promo-window__description-text">
+          Elliot (Rami Malek) to cierpiący na fobię społeczną, niewyróżniający
+          się z tłumu, skryty w sobie programista. Jest specjalistą od
+          cyberbezpieczeństwa w dużej korporacji AllSafe.
+        </p>
       </div>
     </div>
   );
